748ed4f8f76705f5e2495586ee7eef85a2c3eaf4
jasanihardik <jasanihardik11@gmail.com>
2018-11-09 16:06:05 -0500

    Initial Commit

---
+++ Activity Indicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activity Indicator
+//
+//  Created by JASANI HARDIK on 2018-11-09.
+//  Copyright © 2018 JASANI HARDIK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
